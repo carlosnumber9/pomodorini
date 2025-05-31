ef3a359a61ae8b2a2dca9543e3d3b3ded8dd17f2
Carlos. <cpinedocsb@gmail.com>
2025-05-31 22:52:53 +0000

    feat: App init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Pomodorini</h1>
+      <div className="card">
+        <input type='number' />
+        <button> Start </button>
+        <p>
+          Enter the number of hours to work on and click on start
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
